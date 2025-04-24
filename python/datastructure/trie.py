6ee97572810ace524ff474c7ef9f561138295675
wu-uk <wuyukai99@gmail.com>
2025-04-25 02:28:46 +0800

    Update trie.py

--- python/datastructure/trie.py
+++ python/datastructure/trie.py
@@ -1,22 +1,26 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict() # 子节点
+        self.is_end = False # 是否为单词结尾
+        self.word = 0 # 以当前节点为结尾的单词数
+        self.count = 0 # 当前节点作为前缀的单词数
+        
 class Trie:
     def __init__(self):
-        self.children = dict()
-        self.is_end = False
-        self.word = 0
-        self.count = 0
+        self.node = TrieNode()
     
     def insert(self, word):
-        node = self
+        node = self.node
         for c in word:
             if c not in node.children:
-                node.children[c] = Trie()
+                node.children[c] = TrieNode()
             node = node.children[c]
             node.count += 1
         node.is_end = True
         node.word += 1
     
     def search(self, word):
-        node = self
+        node = self.node
         for c in word:
             if c not in node.children:
                 return 0
